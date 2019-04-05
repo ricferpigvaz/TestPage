444622037ae46a935e339dd357be59cb15e19ffd
Ricardo Fernando Pigeón Vázquez <ricardo.pigeon@corusconsulting.com>
2019-04-05 16:11:07 -0600

    carga de proyecto

---
+++ AspWebSiteTest/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspWebSiteTest.Startup))]
+namespace AspWebSiteTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
